c0e2cab81da09b3b5181e6902a9895402a7dd7d3
Hieu Phan <64969360+Pogchop@users.noreply.github.com>
2021-01-22 09:19:46 +0700

    Add files via upload

---
+++ Cylinder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    public class Cylinder
+    {
+        public double radius;
+        public double height;
+        public double BaseArea;
+        public double LateralArea;
+        public double TotalArea;
+        public double Volume;
+        public void Process()
+        {
+            BaseArea = radius * radius * Math.PI;
+            LateralArea = 2 * Math.PI * radius * height;
+            TotalArea = 2 * Math.PI * radius * (height + radius);
+            Volume = Math.PI * radius * radius * height;
+        }
+        public void Result()
+        {
+            Console.WriteLine("Cylinder Characteristic");
+            Console.WriteLine("Radius {0}, Height{1}", radius, height);
+            Console.WriteLine("Base {0}, Lateral {1}, Total {2}, Volume{3}", BaseArea, LateralArea, TotalArea, Volume);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cylinder test = new Cylinder();
+            Console.WriteLine("Enter the dimension of the cylinder");
+            Console.WriteLine("Radius: ");
+            test.radius = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Height: ");
+            test.height = Convert.ToDouble(Console.ReadLine());
+            test.Process();
+            test.Result();
+            Console.ReadKey();
+        }
+    }
+}
